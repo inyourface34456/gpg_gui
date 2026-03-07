b269109df743387e93beefc5077cb35ffb27bac4
inyourface3445 <trenten46@gmail.com>
2026-03-07 03:58:26 -0500

    Deploy

--- publish.sh
+++ publish.sh
@@ -4,7 +4,10 @@ trunk build --release
 # Copy dist contents to a temp location
 cp -r dist/ /tmp/gh-pages-dist/
 
-# Switch to gh-pages, wipe it, replace with new build
+# Switch to gh-pages, wipe it, replace with new build\
+git add .
+git commit -m $1
+git push origin main
 git checkout gh-pages
 cp -r /tmp/gh-pages-dist/* .
 git add .
