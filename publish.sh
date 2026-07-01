dcc62705078a280f0a59c5115bd229b41cb37eb6
inyourface3445 <trenten46@gmail.com>
2026-07-01 02:21:31 -0400

    new publish workflow test 2

--- publish.sh
+++ publish.sh
@@ -4,27 +4,27 @@ find . -name '*.rs' -exec sed -i 's/[ \t]*$//' {} +
 cargo fmt
 trunk build --release
 
-# Copy dist contents to a temp location
-rm -rf /tmp/target-cache
-mkdir /tmp/target-cache
-mv target /tmp/target-cache
+CACHE_DIR="../.build-cache"
+mkdir -p "$CACHE_DIR"
+rm -rf "$CACHE_DIR/target"
+mv target "$CACHE_DIR/"
 
 rm -rf /tmp/gh-pages-dist
 mkdir /tmp/gh-pages-dist
-cp dist/* /tmp/gh-pages-dist/
+cp -r dist/* /tmp/gh-pages-dist/
 
-# commit main so i doint acadently wipe miain branch
 git add .
 git commit --allow-empty -m "$1"
 git push
 
-# Switch to gh-pages, wipe it, replace with new build
 git checkout gh-pages
 rm -rf * target
 cp -r /tmp/gh-pages-dist/* .
 git add .
 git commit --allow-empty -m "$1"
 git push origin gh-pages
+
 git checkout main
-rm -rf target
-mv /tmp/target-cache/target .
+mv "$CACHE_DIR/target" .
+
+rm -rf /tmp/gh-pages-dist
